a513a0091c1b228392125881741d84d486f5150a
SuminGod <34741517+SuminGod@users.noreply.github.com>
2026-01-20 06:18:56 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,8 +2,8 @@
 from .base import router as base_router
 from .income import router as income_router
 from .expense import router as expense_router
+from .debts import router as debts_router
 from .reports import router as reports_router
-from .bets import router as bets_router
 from .firm_payment import router as firm_payment_router
 from .admin import router as admin_router
 from .delete_records import router as delete_router
@@ -11,10 +11,11 @@ from .delete_records import router as delete_router
 __all__ = [
     'base_router', 
     'income_router', 
-    'expense_router', 
-    'reports_router', 
-    'bets_router', 
+    'expense_router',
+    'debts_router',
+    'reports_router',  
     'firm_payment_router',
     'admin_router',
     'delete_router'
 ]
+
